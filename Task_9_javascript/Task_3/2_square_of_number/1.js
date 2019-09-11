8e3c843cc59a075d98c701a99ff1a9f5758d5cdf
TarasAlokhin <53905893+TarasAlokhin@users.noreply.github.com>
2019-09-11 14:13:47 +0300

    new tasks

---
+++ Task_9_javascript/Task_3/2_square_of_number/1.js
@@ -0,0 +1,11 @@
+function pow2() {
+
+  // document.getElementById('inp1').value
+var num1; //змінна
+num1 = document.getElementById('inp1').value
+  alert(num1*num1);
+  num1 = 7;
+  var b;
+  b = 5;
+  alert (num1 + b) ;
+}
---
+++ Task_9_javascript/Task_3/3_form_input_rang_textarea_checkbox/3.js
@@ -0,0 +1,56 @@
+
+// document.querySelector('button').onclick = () => {
+//   console.log(document.querySelector('#one').value);
+// //style
+// document.querySelector('button').style.backgroundColor=
+// document.querySelector('#one').value
+//
+// }
+//
+// document.querySelector('#one').oninput = ()  => {
+//   console.log(document.querySelector('#one').value);
+//   document.querySelector('span').innerHTML = document.querySelector('#one').value
+// }
+
+// document.querySelector('#btn-1').onclick = () => {
+//   console.log(document.querySelector('#i2').value);
+//   let myCheckBox = document.querySelector('#i2');
+//   console.log(myCheckBox.checked);
+//
+//   if (myCheckBox.cheked) {
+// console.log('Натиснутий');
+// }
+//   else {
+// console.log('не натиснутий');
+//
+// }
+// }
+//
+// document.querySelector('#btn-2').onclick = (event) => {
+// event.preventDefault();
+// // let text = document.querySelector('#two');
+// // console.log(text.value);
+// // text.value = 'one';
+// let form = document.querySelector('form');
+// console.log(form);
+// console.log(form.elements.two.value);
+// console.log(form.elements.three.value);
+// }
+
+// Home work
+// Task 1. Создайте button - при нажатии на него выводите alert с номером задачи.
+document.querySelector('#btn-1').onclick = () => {
+alert ('Task_1');
+}
+// // Task 2.
+// // Создайте input type=button - при нажатии на него выводите alert с номером задачи.
+document.querySelector('#btn-2').onclick = () => {
+alert ('Task_2');
+}
+// // Task 3.
+// // Создайте p - при нажатии на него выводите alert с номером задачи.
+document.querySelector('#p1').onclick = () => {
+alert ('Task_3');
+}
+// Task 4.
+// Создайте input(checkbox) и button - при нажатии на него выводите true если checkbox выбран и false если нет.
