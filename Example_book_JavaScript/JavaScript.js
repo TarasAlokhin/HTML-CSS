7702ee0ef2b8fd01156d7cdddd6ebc423434af3a
TarasAlokhin <53905893+TarasAlokhin@users.noreply.github.com>
2019-09-29 20:04:02 +0300

    add task_16_BlockAndColors

--- Example_book_JavaScript/JavaScript.js
+++ Example_book_JavaScript/JavaScript.js
@@ -1,14 +1,14 @@
 // Теорема Піфагора
-var points = [{ x: 0, y: 0 }, { x: 1, y: 1 }];
+// var points = [{ x: 0, y: 0 }, { x: 1, y: 1 }];
 
-points.dist = function() {
-  var p1 = this[0];
-  var p2 = this[1];
-  var a = p2.x - p1.x;
-  var b = p2.y - p1.y;
-  return Math.sqrt(a * a + b * b);
-};
-console.log(points.dist()); // відстань між двома точками
+// points.dist = function() {
+//   var p1 = this[0];
+//   var p2 = this[1];
+//   var a = p2.x - p1.x;
+//   var b = p2.y - p1.y;
+//   return Math.sqrt(a * a + b * b);
+// };
+// console.log(points.dist()); // відстань між двома точками
 
 // Функція яка визначає абсолютне значення (модуль числа)
 /*var x = -5;
---
+++ Task_16_BlockAndColors/MyProgect/JS/index.js
@@ -0,0 +1,35 @@
+function ColorBlack() {
+  document.getElementById("screen").style.backgroundColor = "black";
+}
+
+function ColorWhite() {
+  document.getElementById("screen").style.backgroundColor = "white";
+}
+
+function ColorRed() {
+  document.getElementById("screen").style.backgroundColor = "red";
+}
+
+function ColorBlue() {
+  document.getElementById("screen").style.backgroundColor = "blue";
+}
+
+function ColorYellow() {
+  document.getElementById("screen").style.backgroundColor = "yellow";
+}
+
+function ColorGray() {
+  document.getElementById("screen").style.backgroundColor = "gray";
+}
+
+function ColorBrown() {
+  document.getElementById("screen").style.backgroundColor = "brown";
+}
+
+function ColorCoral() {
+  document.getElementById("screen").style.backgroundColor = "coral";
+}
+
+function ColorGreen() {
+  document.getElementById("screen").style.backgroundColor = "green";
+}
