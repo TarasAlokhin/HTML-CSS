843b87de2f57d61e801062c79aabe1ef66a1325a
TarasAlokhin <53905893+TarasAlokhin@users.noreply.github.com>
2019-10-06 22:33:05 +0300

    add RexExp

--- Task_9_javascript/Task_3/2_square_of_number/1.js
+++ Task_9_javascript/Task_3/2_square_of_number/1.js
@@ -1,11 +1,10 @@
 function pow2() {
-
   // document.getElementById('inp1').value
-var num1; //змінна
-num1 = document.getElementById('inp1').value
-  alert(num1*num1);
+  var num1; //змінна
+  num1 = document.getElementById("inp1").value;
+  alert(num1 * num1);
   num1 = 7;
   var b;
   b = 5;
-  alert (num1 + b) ;
+  document.write(num1 + b);
 }
